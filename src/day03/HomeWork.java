d95ffd0dd9cfe517c319d59fecd27fab58ca6486
xuelei12345 <1347136516@qq.com>
2020-06-17 08:58:52 +0800

    薛磊完成作业提交上传

--- src/day03/HomeWork.java
+++ src/day03/HomeWork.java
@@ -3,7 +3,12 @@ package day03;
 public class HomeWork {
     public static void main(String[] args){
         //打印99乘法表
+        System.out.println("打印成功");
+
 
         //打印等腰三角形
+        System.out.println("打印成功");
+
+        System.out.println("作业完成");
     }
 }
